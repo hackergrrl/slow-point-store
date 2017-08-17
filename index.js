b653e590edf6baf4c914bfc4cab36702099c59d3
Stephen Whitmore <sww@eight45.net>
2017-08-17 14:03:54 -0700

    create dir on store init

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+var fs = require('fs')
+var mkdirp = require('mkdirp')
 var builtinTypes = require('comparable-storable-types')
 
 module.exports = FixedGridPointStore
 
-function FixedGridPointStore (pointType, valueType) {
+function FixedGridPointStore (dir, pointType, valueType) {
   if (!(this instanceof FixedGridPointStore)) return new FixedGridPointStore()
   if (!pointType) throw new Error('must provide "pointType" parameter')
   if (!valueType) throw new Error('must provide "valueType" parameter')
 
+  this.dir = dir
   this.pointType = builtinTypes(pointType)
   this.valueType = builtinTypes(valueType)
+
+  // Let it throw if 'dir' is no good. Doing this sync ensures the store is
+  // ready to go immediately after it's created.
+  mkdirp.sync(dir)
 }
 
 FixedGridPointStore.prototype.insert = function (pt, value, cb) {
